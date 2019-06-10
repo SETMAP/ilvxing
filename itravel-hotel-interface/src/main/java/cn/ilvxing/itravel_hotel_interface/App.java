7f32e688e7ab676fe0709e19327424a09f87edcf
ccq <ccq@DESKTOP-PMF5RQP>
2019-06-10 14:50:26 +0800

    itravel

---
+++ itravel-hotel-interface/src/main/java/cn/ilvxing/itravel_hotel_interface/App.java
@@ -0,0 +1,13 @@
+package cn.ilvxing.itravel_hotel_interface;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
