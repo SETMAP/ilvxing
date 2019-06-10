bb8532aacf248178ebaa4abd23b43029cf85e50e
ccq <ccq@DESKTOP-PMF5RQP>
2019-06-10 14:54:09 +0800

    itravel

---
+++ itravel-pay-server/src/main/java/cn/ilvxing/itravel_pay_server/App.java
@@ -0,0 +1,13 @@
+package cn.ilvxing.itravel_pay_server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ itravel-user-commons/src/main/java/cn/ilvxing/itravel_commons/App.java
@@ -0,0 +1,13 @@
+package cn.ilvxing.itravel_commons;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
